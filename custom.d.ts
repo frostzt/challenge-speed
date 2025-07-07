92d052f4109074a66a9c0230d10717125ff66ad0
frostzt <aidenfrostbite@gmail.com>
2025-07-07 16:39:37 +0530

    chore: envs

--- custom.d.ts
+++ custom.d.ts
@@ -1,9 +1,15 @@
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            PORT?: string;
-        }
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT?: string;
+
+      // Postgres
+      PG_HOST?: string;
+      PG_PORT?: string;
+      PG_USER?: string;
+      PG_PASS?: string;
     }
+  }
 }
 
-export {}
+export {};
--- src/data-source.ts
+++ src/data-source.ts
@@ -1,16 +1,16 @@
-import {DataSource} from "typeorm";
-import {Contact} from "./identification/entities/contact.entity";
+import { DataSource } from "typeorm";
+import { Contact } from "./identification/entities/contact.entity";
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "postgres",
-    synchronize: true,
-    logging: true,
-    entities: [Contact],
-    subscribers: [],
-    migrations: [],
-})
+  type: "postgres",
+  host: process.env.PG_HOST,
+  port: Number(process.env.PG_PORT),
+  username: process.env.PG_USER,
+  password: process.env.PG_PASS,
+  database: "postgres",
+  synchronize: true,
+  logging: true,
+  entities: [Contact],
+  subscribers: [],
+  migrations: [],
+});
